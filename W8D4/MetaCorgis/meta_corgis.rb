2e4069c496a5a962e32e1f6df291cd7ca1e4d2aa
bbennett <tbbennett1@gmail.com>
2019-07-18 08:01:46 -0700

    Update MetaCorgis

--- W8D4/MetaCorgis/meta_corgis.rb
+++ W8D4/MetaCorgis/meta_corgis.rb
@@ -111,12 +111,21 @@ class MetaCorgiSnacks
 
   def method_missing(name, *args)
     # Your code goes here...
-    method_name = name.to_s
-    if 
+    info = @snack_box.send("get_#{name}_info", @box_id)
+    tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
+    name = "#{name.to_s.split('_').map(&:capitalize).join(' ')}"
+    result = "#{name}: #{info}: #{tastiness} "
+    tastiness > 30 ? "* #{result}" : result
   end
 
 
   def self.define_snack(name)
-    # Your code goes here...
+    define_method(name) do
+      info = @snack_box.send("get_#{name}_info", @box_id)
+      tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
+      display_name = "#{name.split('_').map(&:capitalize).join(' ')}"
+      result = "#{display_name}: #{info}: #{tastiness}"
+      tastiness > 30 ? "* #{result}" : result
+    end
   end
 end
